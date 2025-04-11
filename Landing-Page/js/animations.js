3c3d8f60fd25633f122c8e2da8581f3c659d90a5
araldev <arturo.r2d2.dev@gmail.com>
2025-04-11 16:19:17 +0200

    Added scroll transition

---
+++ Landing-Page/js/animations.js
@@ -0,0 +1,24 @@
+(function() {
+// :not(first-child) en la sección del elemento de la animación y hace lo mismo
+
+    // const height = window.visualViewport
+    //     ? window.visualViewport.height
+    //     : window.innerHeight;
+    // const $elements = document.querySelectorAll("main > *");
+
+    // const checkScroll = () => {
+    //     if (window.scrollY < height / 2) {
+    //         $elements.forEach(el => {
+    //             el.style.animation = "none";
+    //         });
+    //     } else {
+    //         $elements.forEach(el => {
+    //             el.style.animation = "";
+    //         });
+    //     }
+    // }
+
+    // checkScroll();
+
+    // window.addEventListener("scroll", checkScroll)
+})();
