4f8badd040530ab6140735c8acd0a19ce9cbb0f4
araldev <arturo.r2d2.dev@gmail.com>
2025-08-25 12:22:35 +0200

    setup config

---
+++ apps/backend/config/jwtConfig.ts
@@ -0,0 +1,4 @@
+export const {
+  REFRESH_TOKEN_SECRET,
+  ACCESS_TOKEN_SECRET,
+} = process.env
--- apps/backend/config/serverConfig.ts
+++ apps/backend/config/serverConfig.ts
@@ -1,4 +1,20 @@
+/* ---------------  REDIS SETUP --------------- */
+
 export const {
   PORT = 1234,
-  NODE_ENV = 'development'
+  NODE_ENV = 'development',
+  REDIS_USERNAME = 'default',
+  REDIS_PASSWORD = '',
+  REDIS_HOST = 'localhost',
 } = process.env
+
+export const REDIS_PORT = Number(process.env.REDIS_PORT ) ?? 3306
+
+export const REDIS_CONFIG = {
+  username: REDIS_USERNAME,
+  password: REDIS_PASSWORD,
+  socket: {
+    host: REDIS_HOST,
+    port: REDIS_PORT
+  }
+}
