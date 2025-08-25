51c53994fa789403ec8df459dfa6300ae23ea4b2
araldev <arturo.r2d2.dev@gmail.com>
2025-08-25 12:23:26 +0200

    updated shared bounded context

--- apps/backend/src/shared/errors/application/CreateCustomError.ts
+++ apps/backend/src/shared/errors/application/CreateCustomError.ts
@@ -2,48 +2,48 @@ import {CustomError} from '@src/shared/errors/domain/aggregate/CustomErrors'
 import {CodeError} from '@src/shared/errors/domain/repositories/CustomErrorsInterface'
 
 export class CreateCustomError {
-   static INTERNAL_ERROR() {
-      return new CustomError({
+   static INTERNAL_ERROR(): never {
+      throw new CustomError({
         code: CodeError.INTERNAL_ERROR,
         message: "Server Issues",
         statusCode: 500
       })
     }
 
-    static USER_NOT_FOUND() {
-      return new CustomError({
+    static USER_NOT_FOUND(): never {
+      throw new CustomError({
         code: CodeError.USER_NOT_FOUND,
         message: "User not found",
         statusCode: 404
       })
     }
 
-    static INVALID_CREDENTIALS() {
-      return new CustomError({
+    static INVALID_CREDENTIALS(): never {
+      throw new CustomError({
         code: CodeError.INVALID_CREDENTIALS,
         message: "Insert a valid username or password",
         statusCode: 401
       })
     }
 
-    static INVALID_EMAIL() {
-      return new CustomError({
+    static INVALID_EMAIL(): never {
+      throw new CustomError({
         code: CodeError.INVALID_EMAIL,
         message: "Invalid Email",
         statusCode: 404
       })
     }
 
-    static USERNAME_TAKEN() {
-      return new CustomError({
+    static USERNAME_TAKEN(): never {
+      throw new CustomError({
         code: CodeError.USERNAME_TAKEN,
         message: "This username already exists",
         statusCode: 400
       })
     }
 
-    static UNSECURE_PASSWORD() {
-      return new CustomError({
+    static UNSECURE_PASSWORD(): never {
+      throw new CustomError({
         code: CodeError.UNSECURE_PASSWORD,
         message: "Your password is unsecure",
         statusCode: 400
