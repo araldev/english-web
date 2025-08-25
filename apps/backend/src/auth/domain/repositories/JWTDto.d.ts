2be610c2d03653f7014e3625b2286bc0704e256c
araldev <arturo.r2d2.dev@gmail.com>
2025-08-25 12:21:00 +0200

    delete useless files

--- apps/backend/src/auth/domain/repositories/JWTDto.d.ts
+++
@@ -1,6 +0,0 @@
-export interface JWTDto {
-  id: string
-  username:string
-  email:string
-  role:
-}
--- apps/backend/src/auth/infrastructure/integrations/jwtAdapter.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export class jwtAdapter {
-  
-  
-  constructor() {
-    this.token = null
-  }
-}
