84a50eb13be38cd57b9c5742467f7bfb0cc963df
araldev <arturo.r2d2.dev@gmail.com>
2025-08-26 02:14:32 +0200

    added new metod

--- apps/backend/src/user/application/port/UserManagmentDto.d.ts
+++ apps/backend/src/user/application/port/UserManagmentDto.d.ts
@@ -1,5 +1,17 @@
+import type { authUserCredentialRegister } from "@/src/auth/domain/repositories/AuthSessionDto"
+import type { UserIdDto, UserModel, UserModelUpdate, UsernameDto } from "../../domain/repositories/UserModel"
 import type { UserRepositoryDto } from "./UserRepositoryDto"
 
 export interface UserManagmentDto {
   UserRepository: UserRepositoryDto
+
+  findById({userId}: {userId: UserIdDto}): Promise<UserModel>
+
+  findByUsername({username}: {username: UsernameDto}): Promise<UserModel>
+  
+  create({user}: {user: authUserCredentialRegister}): Promise<UserModel> 
+  
+  update({user}:{user: UserModelUpdate}): Promise<UserModel> 
+
+  delete({userId}: {userId: UserIdDto}): Promise<true |false> 
 }
--- apps/backend/src/user/application/uses_cases/UserManagment.ts
+++ apps/backend/src/user/application/uses_cases/UserManagment.ts
@@ -1,9 +1,10 @@
-import type { UserModel, UserModelUpdate, UserIdDto } from '@src/user/domain/repositories/UserModel'
-import { idSchema, userSchema, userUpdateSchema } from '@src/user/domain/repositories/UserModel'
+import type { UserModel, UserModelUpdate, UserIdDto, UsernameDto } from '@src/user/domain/repositories/UserModel'
+import { idSchema, usernameSchema, userSchema, userUpdateSchema } from '@src/user/domain/services/userSchema'
 import type {UserRepositoryDto} from '@src/user/application/port/UserRepositoryDto'
 import type {UserManagmentDto} from '@src/user/application/port/UserManagmentDto'
 import { CreateCustomError } from '@/src/shared/errors/application/CreateCustomError'
 import {User} from '@src/user/domain/aggregate/User'
+import type { authUserCredentialRegister } from '@/src/auth/domain/repositories/AuthSessionDto'
 
 export class UserManagment implements UserManagmentDto {
   UserRepository: UserRepositoryDto
@@ -16,16 +17,28 @@ export class UserManagment implements UserManagmentDto {
   async findById({userId}: {userId: UserIdDto}): Promise<UserModel> {
     if(!userId) CreateCustomError.USER_NOT_FOUND()
 
-    idSchema.parseAsync(userId)
+    const idParse = await idSchema.parseAsync(userId)
 
-    const user = await this.UserRepository.findById({userId})
+    const user = await this.UserRepository.findById({userId: idParse})
 
     if(user != null) return user
     
     CreateCustomError.USER_NOT_FOUND()
   }
 
-  async create({user}: {user: UserModel}): Promise<UserModel> {
+  async findByUsername({username}: {username: UsernameDto}): Promise<UserModel> {
+    if(!username) CreateCustomError.USER_NOT_FOUND()
+
+    const userParse = await usernameSchema.parseAsync(username)
+
+    const user = await this.UserRepository.findByUserName({username: userParse})
+
+    if(user != null) return user
+    
+    CreateCustomError.USER_NOT_FOUND()
+  }
+
+  async create({user}: {user: authUserCredentialRegister}): Promise<UserModel> {
     if(!user)CreateCustomError.USER_NOT_FOUND()
 
     const newUser = User.create({user})
@@ -54,9 +67,9 @@ export class UserManagment implements UserManagmentDto {
   async delete({userId}: {userId: UserIdDto}): Promise<true |false> {
     if(!userId || !this.UserRepository) CreateCustomError.USER_NOT_FOUND()
 
-    idSchema.parseAsync(userId)
+    const idParse = await idSchema.parseAsync(userId)
     
-    const userFinded = this.UserRepository.findById({userId: userId})
+    const userFinded = this.UserRepository.findById({userId: idParse})
 
     if(!userFinded) CreateCustomError.USER_NOT_FOUND()
 
