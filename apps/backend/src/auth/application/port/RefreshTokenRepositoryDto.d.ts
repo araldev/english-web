f165eee060fc9812f3e56e79020acca15011f23f
araldev <arturo.r2d2.dev@gmail.com>
2025-08-26 02:11:15 +0200

    delete useless files

--- apps/backend/src/auth/application/port/RefreshTokenRepositoryDto.d.ts
+++
@@ -1,8 +0,0 @@
-import {RefreshTokenIdDto, RefreshTokenDto} from '@src/auth/domain/repositories/'
-
-export interface RefreshTokenRepositoryDto {
-  findById({id}: {id: RefreshTokenIdDto}): Promise<RefreshTokenDto | null>
-  create({refreshToken}: {refreshToken: RefreshTokenDto}): Promise<RefreshTokenDto> 
-  update({id, refreshToken}: {id: RefreshTokenIdDto, refreshToken: RefreshTokenDto}): Promise<RefreshTokenDto>
-  delete({id}: {id: RefreshTokenIdDto}): Promise<boolean>
-}
