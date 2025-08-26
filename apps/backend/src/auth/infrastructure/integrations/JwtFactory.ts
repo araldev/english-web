6f1fb974b667f7abafd35f8dc4a95b1373723aa7
araldev <arturo.r2d2.dev@gmail.com>
2025-08-26 02:13:49 +0200

    become static class into instance class

--- apps/backend/src/auth/infrastructure/integrations/JwtFactory.ts
+++ apps/backend/src/auth/infrastructure/integrations/JwtFactory.ts
@@ -1,23 +1,11 @@
 import { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } from '@/config/jwtConfig'
-import type {JwtModelDto, JwtIdDto, JwtDto, JwtExpDto, JwtIatDto, JwtPayloadDto} from '@/src/auth/domain/repositories/JwtDto'
+import type {JwtModelDto, JwtDto, JwtPayloadDto} from '@/src/auth/domain/repositories/JwtDto'
 import {jwtModelSchema, jwtPayloadSchema} from '@/src/auth/domain/services/jwtSchemas'
 import { CreateCustomError } from '@/src/shared/errors/application/CreateCustomError'
 import jwt from 'jsonwebtoken'
 
-export class Jwt {
-  id: JwtIdDto
-  token: JwtDto
-  iat: JwtIatDto
-  exp: JwtExpDto
-
-  private constructor ({id, token, iat, exp}: JwtModelDto) {
-    this.id = id
-    this.token = token
-    this.iat = iat
-    this.exp = exp 
-  }
-
-  static async createRefresh(payload: JwtPayloadDto): Promise<JwtModelDto> {
+export class JwtFactory  {
+  async createRefresh(payload: JwtPayloadDto): Promise<JwtModelDto> {
     const payloadParse = await jwtPayloadSchema.parseAsync(payload)
     if(!payloadParse) CreateCustomError.INVALID_CREDENTIALS()
     if(!REFRESH_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
@@ -33,7 +21,7 @@ export class Jwt {
     return await jwtModelSchema.parseAsync(token)
   }
 
-  static async validateRefresh({token}: {token: JwtDto}) {
+  async validateRefresh({token}: {token: JwtDto}) {
     if(!token) CreateCustomError.INVALID_CREDENTIALS()
     if(!REFRESH_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
     
@@ -45,7 +33,7 @@ export class Jwt {
     return await jwtModelSchema.parseAsync(tokenVerified)
   }
 
-  static async createAccess(payload: JwtPayloadDto): Promise<JwtModelDto> {
+  async createAccess(payload: JwtPayloadDto): Promise<JwtModelDto> {
     const payloadParse = await jwtPayloadSchema.parseAsync(payload)
     if(!payloadParse) CreateCustomError.INVALID_CREDENTIALS()
     if(!ACCESS_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
@@ -61,7 +49,7 @@ export class Jwt {
     return await jwtModelSchema.parseAsync(token)
   }
 
-  static async validateAccess({token}: {token: JwtDto}) {
+  async validateAccess({token}: {token: JwtDto}) {
     if(!token) CreateCustomError.INVALID_CREDENTIALS()
     if(!ACCESS_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
     
