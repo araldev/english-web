f02e7565dae96c87b84c1decb0c31bcb9169dc70
araldev <arturo.r2d2.dev@gmail.com>
2025-09-02 16:29:49 +0200

    create error middleware

--- apps/backend/src/shared/errors/infrastructure/middlewares/errorMiddleware.ts
+++ apps/backend/src/shared/errors/infrastructure/middlewares/errorMiddleware.ts
@@ -1,8 +1,20 @@
 import { CustomError } from '@src/shared/errors/domain/aggregate/CustomErrors.js'
-import type {CustomErrorInterface} from '@src/shared/errors/domain/repositories/CustomErrorsInterface.js'
+import type { CustomErrorInterface } from '@src/shared/errors/domain/repositories/CustomErrorsInterface.js'
+import type { Response, Request, NextFunction } from 'express'
 
-export function errorMiddleware(err: CustomErrorInterface) {
-  if(err instanceof CustomError){
-    throw new Error(err.message)
+export function errorMiddleware(err: CustomErrorInterface | Error, req: Request, res: Response, next: NextFunction) {
+  if (err instanceof CustomError) {
+    // Respuesta con el mensaje y un status code personalizado (ej: 400)
+    return res.status(err.statusCode || 400).json({
+      error: true,
+      code: err.code,
+      message: err.message,
+      statusCode: err.statusCode || 400,
+    })
   }
+
+  return res.status( 400 ).json({
+      error: true,
+      message: err.message,
+  })
 }
