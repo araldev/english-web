51ab580fd3aa53cb8e700b77c4d9936acb1eac4b
araldev <arturo.r2d2.dev@gmail.com>
2025-09-02 16:31:46 +0200

    added next(error) to manage errors

--- apps/backend/entry_point/api/auth/controllers/authController.ts
+++ apps/backend/entry_point/api/auth/controllers/authController.ts
@@ -6,7 +6,7 @@ import { type JwtModelDto } from "@src/auth/domain/repositories/JwtDto.d.js"
 import { Token } from "@src/auth/domain/services/jwtSchemas.js"
 import { JwtFactory } from "@src/auth/infrastructure/integrations/JwtFactory.js"
 import { CreateCustomError } from "@src/shared/errors/application/CreateCustomError.js"
-import type {Request, Response} from 'express'
+import type {NextFunction, Request, Response} from 'express'
 
 export class AuthController {
   private readonly authUserService: AuthUserService
@@ -25,58 +25,74 @@ export class AuthController {
     this.authUserService = authUserService
     this.tokenClientRepository= tokenClientRepository
   }  
+  
   async register(
     req: Request<unknown, unknown, AuthUserCredentialRegister>, 
-    res: Response
+    res: Response,
+    next: NextFunction
   ) {
-    const {username, password, email} = req.body
-    const userSession = await this.authUserService.register({username, password, email})
-    const {id} = userSession
-
-    const refreshToken = await JwtFactory.createRefresh(userSession)
-    if(!refreshToken) CreateCustomError.INVALID_CREDENTIALS()
-    const {jwtId} = await JwtFactory.validateRefresh({token: refreshToken})
-    this.tokenClientRepository.insert({jwtId, userId: id, refreshToken})
-
-    const accessToken = await JwtFactory.createAccess(userSession)
-    if(!accessToken) CreateCustomError.INVALID_CREDENTIALS()
-
-    res
-    .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken )
-    .cookie(Token.access_token, accessToken, cookieConfig.accessToken )
-    .json({username, password, email, message: 'User registered successfully'})
+    try {
+      const {username, password, email} = req.body
+      const userSession = await this.authUserService.register({username, password, email})
+      const {id} = userSession
+  
+      const refreshToken = await JwtFactory.createRefresh(userSession)
+      if(!refreshToken) CreateCustomError.INVALID_CREDENTIALS()
+      const {jwtId} = await JwtFactory.validateRefresh({token: refreshToken})
+      this.tokenClientRepository.insert({jwtId, userId: id, refreshToken})
+  
+      const accessToken = await JwtFactory.createAccess(userSession)
+      if(!accessToken) CreateCustomError.INVALID_CREDENTIALS()
+  
+      res
+      .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken )
+      .cookie(Token.access_token, accessToken, cookieConfig.accessToken )
+      .json({username, password, email, message: 'User registered successfully'})
+    } catch (error) {
+      next(error)
+    }
   }
 
   async login(
     req: Request<unknown, unknown, AuthUserCredential>, 
-    res: Response
+    res: Response,
+    next: NextFunction
   ) {
-    const {username, password} = req.body
-    const userSession = await this.authUserService.login({username, password})
-    const {email, id} = userSession
-
-    const refreshToken = await JwtFactory.createAccess({id, username, email})
-    const accessToken = await JwtFactory.createAccess(userSession)
-    if(!accessToken || refreshToken) CreateCustomError.INVALID_CREDENTIALS()
-
-    res
-    .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken )
-    .cookie(Token.access_token, accessToken, cookieConfig.accessToken )
-    .json({ username, password, email, message: 'User login successfully'})
+    try {
+      const {username, password} = req.body
+      const userSession = await this.authUserService.login({username, password})
+      const {email, id} = userSession
+  
+      const refreshToken = await JwtFactory.createAccess({id, username, email})
+      const accessToken = await JwtFactory.createAccess(userSession)
+      if(!accessToken || refreshToken) CreateCustomError.INVALID_CREDENTIALS()
+  
+      res
+      .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken )
+      .cookie(Token.access_token, accessToken, cookieConfig.accessToken )
+      .json({ username, password, email, message: 'User login successfully'})
+    } catch (error) {
+      next(error)
+    }
   }
 
   async logout(
     req: Request & {cookies: JwtModelDto},
-    res: Response
+    res: Response,
+    next: NextFunction
   ) {
-    const {refreshToken} = req.cookies
-    const {jwtId, id: userId, username} = await JwtFactory.validateRefresh({token: refreshToken})
-
-    this.tokenClientRepository.delete({jwtId, userId})
-
-    res
-    .clearCookie(Token.refresh_token)
-    .clearCookie(Token.refresh_token)
-    .json({username, message: "Succesful logout"})
+    try {
+      const {refreshToken} = req.cookies
+      const {jwtId, id: userId, username} = await JwtFactory.validateRefresh({token: refreshToken})
+  
+      this.tokenClientRepository.delete({jwtId, userId})
+  
+      res
+      .clearCookie(Token.refresh_token)
+      .clearCookie(Token.refresh_token)
+      .json({username, message: "Succesful logout"})
+    } catch (error) {
+      next(error)
+    }
   }
 }
