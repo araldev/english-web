385341aa103e516d645f74072b36e1bdf5a6b909
araldev <arturo.r2d2.dev@gmail.com>
2025-09-02 16:30:31 +0200

    add turso client with prisma

--- apps/backend/entry_point/api/servers/redisServer.ts
+++ apps/backend/entry_point/api/servers/redisServer.ts
@@ -1,8 +1,12 @@
 import { CreateCustomError } from "@/src/shared/errors/application/CreateCustomError.js"
 import { createServer } from "@api/createServer.js"
 import { JwtRepositoryRedis } from "@src/auth/infrastructure/persistence/JwtRepositoryRedis.js"
+import { UserRepositoryTursoPrisma } from '@/src/user/infrastructure/persistence/UserRepositoryTurso.js'
 
 const tokenClientRepository = await JwtRepositoryRedis.connect({})
 if (!tokenClientRepository) CreateCustomError.INTERNAL_ERROR()
+
+const userClientRepository = await UserRepositoryTursoPrisma.connect()
+if (!userClientRepository) CreateCustomError.INTERNAL_ERROR()
   
 createServer({tokenClientRepository, userClientRepository})
