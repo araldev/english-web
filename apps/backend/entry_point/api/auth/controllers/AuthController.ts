e7501464b9d022b97e22d7332694ad33c9d0c746
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 01:02:46 +0200

    fix json response

--- apps/backend/entry_point/api/auth/controllers/AuthController.ts
+++ apps/backend/entry_point/api/auth/controllers/AuthController.ts
@@ -1,12 +1,12 @@
-import { cookieConfig } from "@config/cookieConfig.js"
-import type { JwtRepositoryDto } from "@src/auth/application/port/JwtRepositoryDto.js"
-import type { AuthUserService } from "@src/auth/application/use-cases/AuthUserService.js"
-import type { AuthUserCredential, AuthUserCredentialRegister } from "@src/auth/domain/repositories/AuthSessionDto.js"
-import { type JwtModelDto } from "@src/auth/domain/repositories/JwtDto.d.js"
-import { Token } from "@src/auth/domain/services/jwtSchemas.js"
-import { JwtFactory } from "@src/auth/infrastructure/integrations/JwtFactory.js"
-import { CreateCustomError } from "@src/shared/errors/application/CreateCustomError.js"
-import type {NextFunction, Request, Response} from 'express'
+import { cookieConfig } from '@config/cookieConfig.js'
+import type { JwtRepositoryDto } from '@src/auth/application/port/JwtRepositoryDto.js'
+import type { AuthUserService } from '@src/auth/application/use-cases/AuthUserService.js'
+import type { AuthUserCredential, AuthUserCredentialRegister, AuthUserSession } from '@src/auth/domain/repositories/AuthSessionDto.js'
+import { type JwtModelDto } from '@src/auth/domain/repositories/JwtDto.d.js'
+import { Token } from '@src/auth/domain/services/jwtSchemas.js'
+import { JwtFactory } from '@src/auth/infrastructure/integrations/JwtFactory.js'
+import { CreateCustomError } from '@src/shared/errors/application/CreateCustomError.js'
+import type { NextFunction, Request, Response } from 'express'
 
 export class AuthController {
   private readonly authUserService: AuthUserService
@@ -28,25 +28,25 @@ export class AuthController {
   
   register = async (
     req: Request<unknown, unknown, AuthUserCredentialRegister>, 
-    res: Response,
+    res: Response<AuthUserSession & {message: string}>,
     next: NextFunction
   ) => {
     try {
-      const {username, password, email} = req.body
-      const userSession = await this.authUserService.register({username, password, email})
-      const {id} = userSession
+      const { username, password, email } = req.body
+      const userSession = await this.authUserService.register({ username, password, email })
+      const { id } = userSession
   
       const refreshToken = await JwtFactory.createRefresh(userSession)
-      const {jwtId} = await JwtFactory.validateRefresh({token: refreshToken})
-      this.tokenClientRepository.insert({jwtId, userId: id, refreshToken})
+      const { jwtId } = await JwtFactory.validateRefresh({ token: refreshToken })
+      this.tokenClientRepository.insert({ jwtId, userId: id, refreshToken })
   
       const accessToken = await JwtFactory.createAccess(userSession)
       if(!refreshToken || !accessToken) CreateCustomError.INVALID_CREDENTIALS()
   
       res
-      .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken )
-      .cookie(Token.access_token, accessToken, cookieConfig.accessToken )
-      .json({username, password, email, message: 'User registered successfully'})
+        .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken)
+        .cookie(Token.access_token, accessToken, cookieConfig.accessToken)
+        .json({ id, username, email, message: 'User registered successfully' })
     } catch (error) {
       next(error)
     }
@@ -54,24 +54,24 @@ export class AuthController {
 
   login = async (
     req: Request<unknown, unknown, AuthUserCredential>, 
-    res: Response,
+    res: Response<AuthUserSession & {message: string}>,
     next: NextFunction
   ) => {
     try {
-      const {username, password} = req.body
-      const userSession = await this.authUserService.login({username, password})
-      const {email, id} = userSession
+      const { username, password } = req.body
+      const userSession = await this.authUserService.login({ username, password })
+      const { email, id } = userSession
   
       const refreshToken = await JwtFactory.createRefresh(userSession)
-      const {jwtId} = await JwtFactory.validateRefresh({token: refreshToken})
-      this.tokenClientRepository.insert({jwtId, userId: id, refreshToken})
+      const { jwtId } = await JwtFactory.validateRefresh({ token: refreshToken })
+      this.tokenClientRepository.insert({ jwtId, userId: id, refreshToken })
       const accessToken = await JwtFactory.createAccess(userSession)
       if(!accessToken || !refreshToken) CreateCustomError.INVALID_CREDENTIALS()
   
       res
-      .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken )
-      .cookie(Token.access_token, accessToken, cookieConfig.accessToken )
-      .json({ username, password, email, message: 'User login successfully'})
+        .cookie(Token.refresh_token, refreshToken, cookieConfig.refreshToken)
+        .cookie(Token.access_token, accessToken, cookieConfig.accessToken)
+        .json({ id, username, email, message: 'User login successfully' })
     } catch (error) {
       next(error)
     }
@@ -83,15 +83,32 @@ export class AuthController {
     next: NextFunction
   ) => {
     try {
-      const {refreshToken} = req.cookies
-      const {jwtId, id: userId, username} = await JwtFactory.validateRefresh({token: refreshToken})
+      const { refreshToken } = req.cookies
+      const { jwtId, id: userId, username } = await JwtFactory.validateRefresh({ token: refreshToken })
   
-      this.tokenClientRepository.delete({jwtId, userId})
+      this.tokenClientRepository.delete({ jwtId, userId })
   
       res
-      .clearCookie(Token.refresh_token)
-      .clearCookie(Token.access_token)
-      .json({username, message: "Succesful logout"})
+        .clearCookie(Token.refresh_token)
+        .clearCookie(Token.access_token)
+        .json({ username, message: 'Succesful logout' })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  me = (
+    req: Request & {cookies: JwtModelDto},
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const userSession = req.session?.user
+      console.log('userSession', userSession)
+      if (userSession) {
+        return res.json(userSession)
+      }
+      return res.status(403).json({ error: true, message: 'You need to login' })
     } catch (error) {
       next(error)
     }
