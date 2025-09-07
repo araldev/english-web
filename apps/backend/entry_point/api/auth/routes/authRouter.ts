043116b09e603829ae9265f8391195b49b1159af
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 01:03:35 +0200

    create /me to allow frontend verify usersession

--- apps/backend/entry_point/api/auth/routes/authRouter.ts
+++ apps/backend/entry_point/api/auth/routes/authRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import {AuthController} from '@/entry_point/api/auth/controllers/AuthController.js'
+import { AuthController } from '@/entry_point/api/auth/controllers/AuthController.js'
 import type { UserRepositoryDto } from '@src/user/application/port/UserRepositoryDto.js'
 import { AuthUserService } from '@/src/auth/application/use-cases/AuthUserService.js'
 import type { JwtRepositoryDto } from '@/src/auth/application/port/JwtRepositoryDto.d.ts'
@@ -13,15 +13,17 @@ export function authRouter(
     tokenClientRepository: JwtRepositoryDto,
     userClientRepository: UserRepositoryDto
   }
- ) {
+) {
   const authRouter = Router()
 
-  const authUserService = new AuthUserService({userClientRepository})
-  const authController = new AuthController({authUserService, tokenClientRepository})
+  const authUserService = new AuthUserService({ userClientRepository })
+  const authController = new AuthController({ authUserService, tokenClientRepository })
 
   authRouter.post('/login', authController.login)
   authRouter.post('/register', authController.register)
   authRouter.post('/logout', authController.logout)
+
+  authRouter.post('/me', authController.me)
   
   return authRouter
 }
