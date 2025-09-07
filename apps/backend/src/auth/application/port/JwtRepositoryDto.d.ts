6998477d40f4d8ad4b1468059da341e58e9cae27
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 01:08:12 +0200

    create connect and disconnect metods

--- apps/backend/src/auth/application/port/JwtRepositoryDto.d.ts
+++ apps/backend/src/auth/application/port/JwtRepositoryDto.d.ts
@@ -1,11 +1,13 @@
 import type { UserIdDto } from '@src/user/domain/repositories/UserModeld.ts'
-import type {JwtDto, JwtCacheRepoDto, JwtIdDto} from '@src/auth/domain/repositories/JwtDto.js'
+import type { JwtDto, JwtCacheRepoDto, JwtIdDto } from '@src/auth/domain/repositories/JwtDto.js'
 
 export interface JwtRepositoryDto {
-  findById({userId, jwtId}: {userId: UserIdDto, jwtId: JwtIdDto}) : Promise<JwtDto | null>
-  findAllById({userId}: {userId: UserIdDto}) : Promise<Array<Record<JwtIdDto, JwtDto>>>
-  insert({jwtId, userId, refreshToken}: JwtCacheRepoDto): Promise<JwtCacheRepoDto> 
-  update({jwtId, userId, refreshToken}: JwtCacheRepoDto): Promise<JwtCacheRepoDto>
-  delete({userId, jwtId}: {userId: UserIdDto, jwtId: JwtIdDto}): Promise<boolean>
-  deleteAll({userId}: {userId: UserIdDto}): Promise<boolean>
+  connect ({ retries, delay }: { retries: number , delay: number }): Promise<boolean | undefined>
+  disconnect(): Promise<boolean>
+  findById({ userId, jwtId }: {userId: UserIdDto, jwtId: JwtIdDto}) : Promise<JwtDto | null>
+  findAllById({ userId }: {userId: UserIdDto}) : Promise<Array<Record<JwtIdDto, JwtDto>>>
+  insert({ jwtId, userId, refreshToken }: JwtCacheRepoDto): Promise<JwtCacheRepoDto> 
+  update({ jwtId, userId, refreshToken }: JwtCacheRepoDto): Promise<JwtCacheRepoDto>
+  delete({ userId, jwtId }: {userId: UserIdDto, jwtId: JwtIdDto}): Promise<boolean>
+  deleteAll({ userId }: {userId: UserIdDto}): Promise<boolean>
 }
