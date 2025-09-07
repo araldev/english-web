bfc8e8d2e7a605e3f2268f9a7a962ccc6d0c7988
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 00:56:58 +0200

    add new rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,25 @@
-import globals from "globals"
-import tseslint from "typescript-eslint"
-import { defineConfig } from "eslint/config"
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,d.ts}"],
-    languageOptions: { globals: {...globals.browser, ...globals.node} },
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,d.ts}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   tseslint.configs.recommended,
   {
     rules: {
-    "no-undef": "warn",
-    semi: ["error", "never"],
-    "prefer-const": "error",
-    "import/no-unresolved": "off",
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/no-unused-vars": "off"
+      'space-in-parens': ['error', 'never'], // nunca espacio
+      'object-curly-spacing': ['error', 'always'], // espacio dentro de llaves
+      'indent': ['error', 2],            // <--- 2 espacios
+      'quotes': ['error', 'single'], 
+      'no-undef': 'warn',
+      semi: ['error', 'never'],
+      'prefer-const': 'error',
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
     },
   }
 ])
