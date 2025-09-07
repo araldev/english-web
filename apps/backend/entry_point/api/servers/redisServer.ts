36603ba964306144e46ead1beaaaadc768dd82a3
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 01:07:54 +0200

    separate create and connect functions

--- apps/backend/entry_point/api/servers/redisServer.ts
+++ apps/backend/entry_point/api/servers/redisServer.ts
@@ -1,12 +1,22 @@
-import { CreateCustomError } from "@/src/shared/errors/application/CreateCustomError.js"
-import { createServer } from "@api/createServer.js"
-import { JwtRepositoryRedis } from "@src/auth/infrastructure/persistence/JwtRepositoryRedis.js"
+import { CreateCustomError } from '@/src/shared/errors/application/CreateCustomError.js'
+import { createServer } from '@api/createServer.js'
+import { JwtRepositoryRedis } from '@src/auth/infrastructure/persistence/JwtRepositoryRedis.js'
 import { UserRepositoryTursoPrisma } from '@/src/user/infrastructure/persistence/UserRepositoryTurso.js'
 
-const tokenClientRepository = await JwtRepositoryRedis.connect({})
-if (!tokenClientRepository) CreateCustomError.INTERNAL_ERROR()
+export async function createRedisServer() {
+  try {
+    const tokenClientRepository = await JwtRepositoryRedis.create()
+    if (!tokenClientRepository) CreateCustomError.INTERNAL_ERROR()
+    const isConnected = await tokenClientRepository.connect({})
+    if (!isConnected) CreateCustomError.INTERNAL_ERROR()
 
-const userClientRepository = await UserRepositoryTursoPrisma.connect()
-if (!userClientRepository) CreateCustomError.INTERNAL_ERROR()
+    const userClientRepository = await UserRepositoryTursoPrisma.connect()
+    if (!userClientRepository) CreateCustomError.INTERNAL_ERROR()
   
-createServer({tokenClientRepository, userClientRepository})
+    createServer({ tokenClientRepository, userClientRepository })
+  } catch (error) {
+    
+  }
+}
+
+createRedisServer()
