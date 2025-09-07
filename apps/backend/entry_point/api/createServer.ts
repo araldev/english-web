2431319dd549e5a14e0b24d11e9027fd26df24b5
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 01:02:20 +0200

    disconnect Redis on Express server shutdown

--- apps/backend/entry_point/api/createServer.ts
+++ apps/backend/entry_point/api/createServer.ts
@@ -1,7 +1,7 @@
-import {PORT} from "@/config/globalConfig.js"
+import { PORT } from '@/config/globalConfig.js'
 import createApp from '@api/createApp.js'
-import type { JwtRepositoryDto } from "@src/auth/application/port/JwtRepositoryDto.js"
-import type { UserRepositoryDto } from "@/src/user/application/port/UserRepositoryDto.d.ts"
+import type { JwtRepositoryDto } from '@src/auth/application/port/JwtRepositoryDto.js'
+import type { UserRepositoryDto } from '@/src/user/application/port/UserRepositoryDto.d.ts'
 
 export function createServer(
   {
@@ -12,27 +12,29 @@ export function createServer(
     tokenClientRepository: JwtRepositoryDto,
     userClientRepository: UserRepositoryDto
   }
- ) {
+) {
   // arrancar servidor
-  const app = createApp({tokenClientRepository, userClientRepository})
+  const app = createApp({ tokenClientRepository, userClientRepository })
   
   const server = app.listen(PORT, () => {
     console.log(`✅ Servidor Express iniciado en http://localhost:${PORT}`)
   })
   
   // detener servidor con Ctrl+C o señales
-  process.on("SIGINT", () => {
-    console.log("🛑 Servidor detenido manualmente (Ctrl+C)")
+  process.on('SIGINT', () => {
+    console.log('🛑 Servidor detenido manualmente (Ctrl+C)')
+    tokenClientRepository.disconnect()
     server.close(() => {
-      console.log("Servidor cerrado correctamente")
+      console.log('Servidor cerrado correctamente')
       process.exit(0)
     })
   })
   
-  process.on("SIGTERM", () => {
-    console.log("🛑 Servidor detenido por señal SIGTERM")
+  process.on('SIGTERM', () => {
+    console.log('🛑 Servidor detenido por señal SIGTERM')
+    tokenClientRepository.disconnect()
     server.close(() => {
-      console.log("Servidor cerrado correctamente")
+      console.log('Servidor cerrado correctamente')
       process.exit(0)
     })
   })
