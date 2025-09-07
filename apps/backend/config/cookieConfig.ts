f7e3d8f7a3cba42e5e1da331063cc055cc41044d
araldev <arturo.r2d2.dev@gmail.com>
2025-09-08 00:58:29 +0200

    modify cookie config

--- apps/backend/config/cookieConfig.ts
+++ apps/backend/config/cookieConfig.ts
@@ -1,5 +1,5 @@
-import type { CookieOptions } from "express"
-import { NODE_ENV } from "@/config/globalConfig.js"
+import type { CookieOptions } from 'express'
+import { NODE_ENV } from '@/config/globalConfig.js'
 
 export const cookieConfig: {
   refreshToken: CookieOptions,
@@ -8,13 +8,15 @@ export const cookieConfig: {
   refreshToken: {
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24 * 30, // 30dias --> milisegundos
-    secure: NODE_ENV === "production" ? true : false,
-    sameSite:"strict"
+    secure: NODE_ENV === 'production',
+    sameSite:'strict',
+    path: '/'
   },
   accessToken: {
     httpOnly: true,
     maxAge: 1000 * 60 * 10, // 10min --> milisegundos
-    secure:NODE_ENV === "production" ? true : false,
-    sameSite:"strict"
+    secure:NODE_ENV === 'production',
+    sameSite:'strict',
+    path: '/'
   }
 }
