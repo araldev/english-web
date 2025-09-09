ded796cdf8f6fba585db7556bd57941d2be8ddd5
araldev <arturo.r2d2.dev@gmail.com>
2025-09-09 19:41:13 +0200

    delete unnecesary conditional

--- apps/backend/src/auth/infrastructure/integrations/JwtFactory.ts
+++ apps/backend/src/auth/infrastructure/integrations/JwtFactory.ts
@@ -1,13 +1,14 @@
 import { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } from '@config/jwtConfig.js'
-import type {JwtDto, JwtPayloadDto} from '@src/auth/domain/repositories/JwtDto.js'
-import {jwtModelSchema, jwtPayloadSchema} from '@src/auth/domain/services/jwtSchemas.js'
+import type { JwtDto, JwtPayloadDto } from '@src/auth/domain/repositories/JwtDto.js'
+import { jwtModelSchema, jwtPayloadSchema } from '@src/auth/domain/services/jwtSchemas.js'
 import { CreateCustomError } from '@src/shared/errors/application/CreateCustomError.js'
 import jwt from 'jsonwebtoken'
+import crypto from 'node:crypto'
+
 
 export class JwtFactory  {
   static async createRefresh(payload: JwtPayloadDto): Promise<JwtDto> {
     const payloadParse = await jwtPayloadSchema.parseAsync(payload)
-    if(!payloadParse) CreateCustomError.INVALID_CREDENTIALS()
     if(!REFRESH_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
 
     const token = jwt.sign(
@@ -18,14 +19,14 @@ export class JwtFactory  {
       },
       REFRESH_TOKEN_SECRET,
       {
-        expiresIn: "30d"
+        expiresIn: '30d'
       }
     )
-
+    
     return token
   }
 
-  static async validateRefresh({token}: {token: JwtDto}) {
+  static async validateRefresh({ token }: {token: JwtDto}) {
     if(!token) CreateCustomError.INVALID_CREDENTIALS()
     if(!REFRESH_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
     
@@ -33,13 +34,12 @@ export class JwtFactory  {
       token,
       REFRESH_TOKEN_SECRET
     )
-    
+      
     return await jwtModelSchema.parseAsync(tokenVerified)
   }
 
   static async createAccess(payload: JwtPayloadDto): Promise<JwtDto> {
     const payloadParse = await jwtPayloadSchema.parseAsync(payload)
-    if(!payloadParse) CreateCustomError.INVALID_CREDENTIALS()
     if(!ACCESS_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
 
     const token = jwt.sign(
@@ -49,14 +49,14 @@ export class JwtFactory  {
       },
       ACCESS_TOKEN_SECRET,
       {
-        expiresIn: "10m"
+        expiresIn: '10m'
       }
     )
-
-    return token
+  
+    return token      
   }
 
-  static async validateAccess({token}: {token: JwtDto}) {
+  static async validateAccess({ token }: {token: JwtDto}) {
     if(!token) CreateCustomError.INVALID_CREDENTIALS()
     if(!ACCESS_TOKEN_SECRET) CreateCustomError.INTERNAL_ERROR()
     
@@ -64,7 +64,7 @@ export class JwtFactory  {
       token,
       ACCESS_TOKEN_SECRET
     )
-    
+      
     return await jwtModelSchema.parseAsync(tokenVerified)
   }
 }
