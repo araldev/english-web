bfb048e1f910b16333b227347a32ae6c0b55e0bf
araldev <arturo.r2d2.dev@gmail.com>
2025-09-09 20:58:52 +0200

    add method(findByEmail)

--- apps/backend/src/user/application/port/UserManagmentDto.d.ts
+++ apps/backend/src/user/application/port/UserManagmentDto.d.ts
@@ -1,14 +1,16 @@
-import type { AuthUserCredentialRegister } from "@src/auth/domain/repositories/AuthSessionDto.js"
-import type { UserIdDto, UserModel, UserModelUpdate, UsernameDto } from "@src/user/domain/repositories/UserModel.js"
+import type { AuthUserCredentialRegister } from '@src/auth/domain/repositories/AuthSessionDto.js'
+import type { EmailDto, UserIdDto, UserModel, UserModelUpdate, UsernameDto } from '@src/user/domain/repositories/UserModel.js'
 
 export interface UserManagmentDto {
-  findById({userId}: {userId: UserIdDto}): Promise<UserModel | null>
+  findByEmail({ email }: {email: EmailDto}): Promise<UserModel | null>
 
-  findByUsername({username}: {username: UsernameDto}): Promise<UserModel | null>
-  
-  create({user}: {user: AuthUserCredentialRegister}): Promise<UserModel> 
-  
-  update({user}:{user: UserModelUpdate}): Promise<UserModel> 
+  findById({ userId }: {userId: UserIdDto}): Promise<UserModel | null>
 
-  delete({userId}: {userId: UserIdDto}): Promise<true |false> 
+  findByUsername({ username }: {username: UsernameDto}): Promise<UserModel | null>
+  
+  create({ user }: {user: AuthUserCredentialRegister}): Promise<UserModel> 
+  
+  update({ user }:{user: UserModelUpdate}): Promise<UserModel> 
+
+  delete({ userId }: {userId: UserIdDto}): Promise<true |false> 
 }
--- apps/backend/src/user/application/port/UserRepositoryDto.d.ts
+++ apps/backend/src/user/application/port/UserRepositoryDto.d.ts
@@ -1,11 +1,12 @@
 import type { AuthUserCredentialRegister } from '@src/auth/domain/repositories/AuthSessionDto.js'
-import type {UserModel, UserModelUpdate, UserIdDto, UsernameDto} from '@src/user/domain/repositories/UserModel.js'
+import type { UserModel, UserModelUpdate, UserIdDto, UsernameDto, EmailDto } from '@src/user/domain/repositories/UserModel.js'
 
 export interface UserRepositoryDto {
-  findById( {userId}: {userId: UserIdDto} ): Promise<UserModel | null>
-  findByUserName( {username}: {username: UsernameDto} ): Promise<UserModel | null>
-  create({user}: {user: AuthUserCredentialRegister}): Promise<UserModel>
-  update({userId, userUpdates}: {userId: UserIdDto, userUpdates:UserModelUpdate}): Promise<UserModel> 
-  delete({userId}: {userId: UserIdDto}): Promise<boolean>
+  findByEmail({ email }: {email: EmailDto}): Promise<UserModel | null>
+  findById({ userId }: {userId: UserIdDto}): Promise<UserModel | null>
+  findByUserName({ username }: {username: UsernameDto}): Promise<UserModel | null>
+  create({ user }: {user: AuthUserCredentialRegister}): Promise<UserModel>
+  update({ userId, userUpdates }: {userId: UserIdDto, userUpdates:UserModelUpdate}): Promise<UserModel> 
+  delete({ userId }: {userId: UserIdDto}): Promise<boolean>
   disconnect(): Promise<boolean>
 }
