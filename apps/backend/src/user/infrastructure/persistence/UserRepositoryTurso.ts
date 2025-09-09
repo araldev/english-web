1b8e44ede6533c0669d9a6bdcb017f0689a5e471
araldev <arturo.r2d2.dev@gmail.com>
2025-09-09 20:59:52 +0200

    implement method (findBYEmail)

--- apps/backend/src/user/infrastructure/persistence/UserRepositoryTurso.ts
+++ apps/backend/src/user/infrastructure/persistence/UserRepositoryTurso.ts
@@ -1,9 +1,9 @@
-import type { AuthUserCredentialRegister } from "@src/auth/domain/repositories/AuthSessionDto"
-import type { UserRepositoryDto } from "@src/user/application/port/UserRepositoryDto.d.js"
-import type { UserIdDto, UserModel, UserModelUpdate, UsernameDto } from "@src/user/domain/repositories/UserModel.d.js"
-import {PrismaClient} from '@/prisma/generated'
+import type { AuthUserCredentialRegister } from '@src/auth/domain/repositories/AuthSessionDto'
+import type { UserRepositoryDto } from '@src/user/application/port/UserRepositoryDto.d.js'
+import type { EmailDto, UserIdDto, UserModel, UserModelUpdate, UsernameDto } from '@src/user/domain/repositories/UserModel.d.js'
+import { PrismaClient } from '@/prisma/generated'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import {TURSO_AUTH_TOKEN, TURSO_DATABASE_URL} from '@/config/prisma-turso-serverConfig.js'
+import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from '@/config/prisma-turso-serverConfig.js'
 
 export class UserRepositoryTursoPrisma implements UserRepositoryDto {
   private readonly prisma: PrismaClient
@@ -25,39 +25,54 @@ export class UserRepositoryTursoPrisma implements UserRepositoryDto {
       authToken: TURSO_AUTH_TOKEN,
     })
     const prisma = new PrismaClient({ adapter })
+    console.log('✅ Conectado a Prisma')
 
-    return new UserRepositoryTursoPrisma({ clientTursoPrisma: prisma})
+    return new UserRepositoryTursoPrisma({ clientTursoPrisma: prisma })
   }
 
-  findById = async ( {userId}: {userId: UserIdDto} ): Promise<UserModel | null> => {
+  findByEmail = async ({ email }:{ email: EmailDto}) => {
+    const userDb = await this.prisma.user.findUnique({
+      where: { email }
+    })
+
+    if (!userDb) return null
+
+    const user = Object.fromEntries(
+      Object.entries(userDb).filter(([_,value]) => value != null)
+    ) as UserModel
+
+    return user
+  }
+
+  findById = async ({ userId }: {userId: UserIdDto}): Promise<UserModel | null> => {
     const userDb = await this.prisma.user.findUnique({
       where: { id: userId }
     })
 
     if (!userDb) return null
 
-  const user = Object.fromEntries(
-    Object.entries(userDb).filter(([_, value]) => value != null)
-  ) as UserModel
+    const user = Object.fromEntries(
+      Object.entries(userDb).filter(([_, value]) => value != null)
+    ) as UserModel
 
-  return user
+    return user
   }
 
-  findByUserName = async ( {username}: {username: UsernameDto} ) => {
+  findByUserName = async ({ username }: {username: UsernameDto}) => {
     const userDb = await this.prisma.user.findUnique({
       where: { username }
     })
 
     if (!userDb) return null
 
-  const user = Object.fromEntries(
-    Object.entries(userDb).filter(([_, value]) => value != null)
-  ) as UserModel
+    const user = Object.fromEntries(
+      Object.entries(userDb).filter(([_, value]) => value != null)
+    ) as UserModel
 
-  return user
+    return user
   }
 
-  create = async ({user}: {user: AuthUserCredentialRegister}) => {
+  create = async ({ user }: {user: AuthUserCredentialRegister}) => {
     const userDb = await this.prisma.user.create({
       data: user
     }) as UserModel
@@ -65,7 +80,7 @@ export class UserRepositoryTursoPrisma implements UserRepositoryDto {
     return userDb
   }
 
-  update = async ({userId, userUpdates}: {userId: UserIdDto, userUpdates:UserModelUpdate}) => {
+  update = async ({ userId, userUpdates }: {userId: UserIdDto, userUpdates:UserModelUpdate}) => {
     const userDbUpdated = await this.prisma.user.update({
       where: {
         id: userId
@@ -76,7 +91,7 @@ export class UserRepositoryTursoPrisma implements UserRepositoryDto {
     return userDbUpdated
   }
 
-  delete = async ({userId}: {userId: UserIdDto}) => {
+  delete = async ({ userId }: {userId: UserIdDto}) => {
     const isDeleted = await this.prisma.user.delete({
       where: { id: userId }
     })
@@ -85,13 +100,13 @@ export class UserRepositoryTursoPrisma implements UserRepositoryDto {
   }
 
   disconnect = async () => {
-  try {    
-    await this.prisma.$disconnect()
-    console.log("⚡ Prisma se ha desconectado...")
-    return true
-  } catch (error) {
-    console.error("❌ Error al desconectar Prisma:", error)
-    return false
+    try {    
+      await this.prisma.$disconnect()
+      console.log('⚡ Prisma se ha desconectado...')
+      return true
+    } catch (error) {
+      console.error('❌ Error al desconectar Prisma:', error)
+      return false
+    }
   }
-}
 }
